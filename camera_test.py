e24aaf359689ad19692ec3971dd87e4d1a70f274
Sibasish Padhihari <sibasishpadhihari@gmail.com>
2026-08-15 23:42:00 +0530

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -1,1 +1,22 @@
+import cv2
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+if not cap.isOpened():
+    print(" Camera not detected")
+    exit()
+
+print("✅ Camera detected")
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("❌ Failed to read frame")
+        break
+
+    cv2.imshow("Camera Test", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
 
